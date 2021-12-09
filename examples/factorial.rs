68fe7e483d34eb8a9c94a7ab60fb5263106caaaa
pione30 <g.pionelish30@gmail.com>
2021-12-10 01:40:42 +0900

    Use `?` operator whenever possible

--- examples/factorial.rs
+++ examples/factorial.rs
@@ -2,11 +2,13 @@ use std::fs;
 use std::str;
 use toysrust::{interpreter, parser};
 
-fn main() {
-    let contents = fs::read("examples/factorial.toys").unwrap();
-    let contents = str::from_utf8(&contents).unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let contents = fs::read("examples/factorial.toys")?;
+    let contents = str::from_utf8(&contents)?;
 
     let (_, program) = parser::program(contents).unwrap();
     let mut interpreter = interpreter::Interpreter::default();
-    interpreter.call_main(program).unwrap();
+    interpreter.call_main(program)?;
+
+    Ok(())
 }
